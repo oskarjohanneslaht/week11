3d433924122c8ee3b18862fdd027bbb4f09a8d86
oskarjohanneslaht <128470468+oskarjohanneslaht@users.noreply.github.com>
2023-12-17 21:05:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(express.json());
 
 
